61426e737073afa4b20aa9840f1e0101f338d86d
Alan Wamsr <alan@wamser.com>
2021-11-20 10:39:30 -0600

    linear

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let sequence: Array = [1,2,3,4,5,6,7,8,9,10]
+
+func linearSearch( for value: Int, list: Array<Int>) -> Bool {
+    for number in list {
+        if number == value {
+            return true
+        }
+    }
+    return false
+}
+
+linearSearch(for: 8, list: sequence)
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# MyPlayground.playground
